dff1b701ea5f3c809984e9fe7ea677feeb3694cb
Ikechukwu Alaeto <ikechukwualaeto@gmail.com>
2026-06-07 00:51:24 +0100

    fix: resolve JSX parsing syntax error for MSO comments in BaseEmailLayout

--- emails/BaseEmailLayout.tsx
+++ emails/BaseEmailLayout.tsx
@@ -117,39 +117,19 @@ export const BaseEmailLayout = ({
                     {/* Footer Socials */}
                     <table width="100%" cellPadding={0} cellSpacing={0} style={{ marginBottom: "20px", textAlign: "center" }}>
                       <tr>
-                        <td align="center">
-                          <span dangerouslySetInnerHTML={{ __html: "<!--[if mso]><table align=\"center\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" style=\"width:250px;\"><tr><td align=\"center\" bgcolor=\"#f4efe6\" style=\"padding:10px 24px;border-radius:30px;\"><![endif]-->" }} />
-                          <table cellPadding={0} cellSpacing={0} style={{ margin: "0 auto", backgroundColor: "#f4efe6", borderRadius: "30px", padding: "10px 24px" }}>
-                            <tr>
-                              <td style={{ padding: "0 10px", verticalAlign: "middle" }}>
-                                <a href="https://www.facebook.com/naturalist.skincare" target="_blank" style={{ display: "block", textDecoration: "none" }}>
-                                  <img src={EMAIL_ASSETS.socialFacebook} alt="Facebook" width="24" height="24" style={{ width: "24px", height: "24px", display: "block", border: 0 }} />
-                                </a>
-                              </td>
-                              <td style={{ padding: "0 10px", verticalAlign: "middle" }}>
-                                <a href="https://www.instagram.com/naturalist.skincare" target="_blank" style={{ display: "block", textDecoration: "none" }}>
-                                  <img src={EMAIL_ASSETS.socialInstagram} alt="Instagram" width="24" height="24" style={{ width: "24px", height: "24px", display: "block", border: 0 }} />
-                                </a>
-                              </td>
-                              <td style={{ padding: "0 10px", verticalAlign: "middle" }}>
-                                <a href="https://x.com/naturalist_skin" target="_blank" style={{ display: "block", textDecoration: "none" }}>
-                                  <img src={EMAIL_ASSETS.socialX} alt="X" width="24" height="24" style={{ width: "24px", height: "24px", display: "block", border: 0 }} />
-                                </a>
-                              </td>
-                              <td style={{ padding: "0 10px", verticalAlign: "middle" }}>
-                                <a href="https://www.tiktok.com/@naturalist.skincare" target="_blank" style={{ display: "block", textDecoration: "none" }}>
-                                  <img src={EMAIL_ASSETS.socialTiktok} alt="TikTok" width="24" height="24" style={{ width: "24px", height: "24px", display: "block", border: 0 }} />
-                                </a>
-                              </td>
-                              <td style={{ padding: "0 10px", verticalAlign: "middle" }}>
-                                <a href="https://www.youtube.com/@naturalist.skincare" target="_blank" style={{ display: "block", textDecoration: "none" }}>
-                                  <img src={EMAIL_ASSETS.socialYoutube} alt="YouTube" width="24" height="24" style={{ width: "24px", height: "24px", display: "block", border: 0 }} />
-                                </a>
-                              </td>
-                            </tr>
-                          </table>
-                          <span dangerouslySetInnerHTML={{ __html: "<!--[if mso]></td></tr></table><![endif]-->" }} />
-                        </td>
+                        <td align="center" dangerouslySetInnerHTML={{ __html: `
+<!--[if mso]><table align="center" border="0" cellspacing="0" cellpadding="0" style="width:250px;"><tr><td align="center" bgcolor="#f4efe6" style="padding:10px 24px;border-radius:30px;"><![endif]-->
+<table cellpadding="0" cellspacing="0" style="margin:0 auto;background-color:#f4efe6;border-radius:30px;padding:10px 24px">
+<tr>
+<td style="padding:0 10px;vertical-align:middle"><a href="https://www.facebook.com/naturalist.skincare" target="_blank" style="display:block;text-decoration:none"><img src="${EMAIL_ASSETS.socialFacebook}" alt="Facebook" width="24" height="24" style="width:24px;height:24px;display:block;border:0" /></a></td>
+<td style="padding:0 10px;vertical-align:middle"><a href="https://www.instagram.com/naturalist.skincare" target="_blank" style="display:block;text-decoration:none"><img src="${EMAIL_ASSETS.socialInstagram}" alt="Instagram" width="24" height="24" style="width:24px;height:24px;display:block;border:0" /></a></td>
+<td style="padding:0 10px;vertical-align:middle"><a href="https://x.com/naturalist_skin" target="_blank" style="display:block;text-decoration:none"><img src="${EMAIL_ASSETS.socialX}" alt="X" width="24" height="24" style="width:24px;height:24px;display:block;border:0" /></a></td>
+<td style="padding:0 10px;vertical-align:middle"><a href="https://www.tiktok.com/@naturalist.skincare" target="_blank" style="display:block;text-decoration:none"><img src="${EMAIL_ASSETS.socialTiktok}" alt="TikTok" width="24" height="24" style="width:24px;height:24px;display:block;border:0" /></a></td>
+<td style="padding:0 10px;vertical-align:middle"><a href="https://www.youtube.com/@naturalist.skincare" target="_blank" style="display:block;text-decoration:none"><img src="${EMAIL_ASSETS.socialYoutube}" alt="YouTube" width="24" height="24" style="width:24px;height:24px;display:block;border:0" /></a></td>
+</tr>
+</table>
+<!--[if mso]></td></tr></table><![endif]-->
+        ` }} />
                       </tr>
                     </table>
 
